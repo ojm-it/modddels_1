fcc4a5be241f5a03c07dc3587d3885d4cee69e0b
ojm-it <80984705+ojm-it@users.noreply.github.com>
2021-11-24 20:46:51 +0100

    commit

---
+++ example/lib/profile.g.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile.dart';
+
+// **************************************************************************
+// ModddelGenerator
+// **************************************************************************
+
+class ProfileModelGen extends ProfileModel {
+  Map<String, dynamic> variables = {};
+  ProfileModelGen() {
+    variables['name'] = super._name;
+    variables['age'] = super._age;
+    variables['codes'] = super._codes;
+    variables['t'] = super._t;
+  }
+  String get name => variables['name'];
+  set name(String name) {
+    super._name = name;
+    variables['name'] = name;
+  }
+
+  int get age => variables['age'];
+  set age(int age) {
+    super._age = age;
+    variables['age'] = age;
+  }
+
+  bool get codes => variables['codes'];
+  set codes(bool codes) {
+    super._codes = codes;
+    variables['codes'] = codes;
+  }
+
+  int get t => variables['t'];
+  set t(int t) {
+    super._t = t;
+    variables['t'] = t;
+  }
+}
