e4b7b59927a287160855396aa645bb877d475b52
ojm-it <80984705+ojm-it@users.noreply.github.com>
2021-12-06 14:40:45 +0100

    commit

--- modddels_annotations/lib/src/entity/sized_list/sized_list_general_entity.dart
+++ modddels_annotations/lib/src/entity/sized_list/sized_list_general_entity.dart
@@ -3,15 +3,15 @@ import 'package:modddels_annotations/src/common.dart';
 import 'package:modddels_annotations/src/entity/common.dart';
 
 abstract class SizedListGeneralEntity<
-    FG extends GeneralFailure,
     FS extends SizeFailure,
+    FG extends GeneralFailure,
     I extends InvalidEntity,
     V extends ValidEntity> extends Modddel<I, V> {
   const SizedListGeneralEntity();
 
   //TODO update this documentation.
 
-  Option<FG> validateGeneral(V valid);
-
   Option<FS> validateSize(int listSize);
+
+  Option<FG> validateGeneral(V valid);
 }
