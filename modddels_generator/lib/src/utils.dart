c94395d5c554cbcd489743c2058726646d61ba5f
ojm-it <80984705+ojm-it@users.noreply.github.com>
2021-12-16 20:01:11 +0100

    commit

--- modddels_generator/lib/src/utils.dart
+++ modddels_generator/lib/src/utils.dart
@@ -174,7 +174,7 @@ class EntityParameter {
   String get invalidNullGeneralFailure {
     final annotation = _invalidNullChecker.annotationsOfExact(parameter).single;
 
-    return annotation.getField('generalFailure')!.toStringValue()!;
+    return annotation.getField('generalFailure').toString();
   }
 }
 
